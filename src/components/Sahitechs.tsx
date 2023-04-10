0e2bbec0acb871e223b4ca18e45102fd30bb24c3
koltukustu <l6918040@std.yildiz.edu.tr>
2023-04-10 03:32:47 +0000

    Update

--- src/components/Sahitechs.tsx
+++ src/components/Sahitechs.tsx
@@ -1,8 +1,8 @@
 function SahiTechs() {
-  return (<img className="w-full pt-0"  id="sahitechs"
+  return <img className="w-full pt-0"  id="sahitechs"
   src="/assets/images/hesap-images/about-us.png"
   alt="SahiTechs"
-  />)
+  />
 ;}
 
 export default SahiTechs;
