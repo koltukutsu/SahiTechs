f6f8717fb90c9e8b5ea4e430b2591c851c6570ec
koltukustu <l6918040@std.yildiz.edu.tr>
2023-04-10 04:31:27 +0000

    Update: inceleyin button is activated

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -12,6 +12,7 @@ const Product = () => {
       <div className={`container max-w-5xl mx-auto m-8`}>
         <h1
           className={`w-full my-2 text-5xl font-bold leading-tight text-center text-primary`}
+          id="introduction"
         >
           {product.title.split(' ').map((word, index) => (
             <span
